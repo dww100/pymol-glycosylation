d2f7b1d299ab62bbbd23e41cc1caf397d4c5624f
David W. Wright <dave.william.wright@gmail.com>
2014-08-21 11:29:19 +0100

    Rewrite to account for new ideas about CONECT records in Pymol

--- pymol_glycos.py
+++ pymol_glycos.py
@@ -58,15 +58,71 @@ def parse_arguments():
 
 pymol.finish_launching()
 
+def get_protein_link_info(sel_text):
+    '''Take a selection text and if it is a ASN, SER or THR residue return 
+    filename for glycan template (n-linked for the former and variation on 
+    o-linked for the latter), the residue type and appropriate link atom name.
+
+    Args:
+        sel_text (str):    Pymol selection text for the protein residue 
+                           candidate for link to carbohydrate
+                         
+    Returns:
+        dict: Contains link template PDB filename, link residue type and link atom 
+
+        Dictionary is of the form:
+
+        {
+            'template': filename of appropriate glycan template,
+            'link_res': link residue type (ASN, SER or THR )
+            'link_atom': selection text for the link atom 
+        }
+
+        If the passed selection text does not find a ASN, SER or THR residue
+        return empty dict 
+         
+    '''
+    
+    output = {}
+    
+    res_type = pymol.cmd.get_model(sel_text, 1).atom[0].resn
+    
+    if res_type in ['SER', 'THR', 'ASN']:
+
+        # Load template glycan (o-linked to a SER or THR as appropriate)
+        # The name of the resulting pymol pbject is stored in glycan_structure
+        if res_type == 'SER':
+            output['template'] = 'ser_o-link.pdb'
+            output['link_res'] = 'SER'
+            output['link_atom'] = 'OG'
+        elif res_type == 'THR':
+            output['template'] = 'thr_o-link.pdb'
+            output['link_res'] = 'THR'
+            output['link_atom'] = 'OG1'
+        else:
+            output['template'] = 'n-link.pdb'
+            output['link_res'] = 'ASN'
+            output['link_atom'] = 'ND2'    
+    
+    return output
+
+def create_gly_link(pro_res_sel, pro_atom, gly_res_sel):
+    
+    pro_sel = pro_res_sel + ' and name ' + pro_atom
+    gly_sel = gly_res_sel + ' and name C1'
+    pymol.cmd.select('b1', pro_sel)
+    pymol.cmd.select('b2', gly_sel)
+    pymol.cmd.bond('b2','b1')
+
 # --------------------
 # Start of main script
 # --------------------
 
 
 # Setting to write CONECT records to PDB
-pymol.cmd.set("pdb_conect_all", "on")
+#pymol.cmd.set("pdb_conect_all", "on")
 # Setting to read CONECT records in PDBs
-pymol.cmd.set("connect_mode", 0)
+pymol.cmd.set("connect_mode", 1)
 
 # Interpret command line arguments
 args = parse_arguments()
@@ -86,14 +142,20 @@ if args.target_file != None:
     target_res_list = yaml.load(f)
 else:
     target_res_list = []
-
-gly2prot_link = {}
     
 if args.exist_file != None:
+    # Read YAML file containing glycan links supposedly present in the original structure
     f = open(args.exist_file, 'r')
     exist_links = yaml.load(f)
     for link in exist_links:
-        gly2prot_link[link['gly_chain'] + str(link['gly_res'])] = link['pro_chain'] + str(link['pro_res']) + link['pro_atm']
+        
+        pro_sel_text = target_structure + ' and chain ' + link['pro_chain'] + ' and resi ' + str(link['pro_res'])
+        link_info = get_protein_link_info(pro_sel_text)
+        
+        if link_info:
+            # Ensure that the glycosydic linkage is included
+            gly_link_res = target_structure + ' and chain ' + link['gly_chain'] + ' and resi ' + str(link['gly_res'])
+            create_gly_link(pro_sel_text, link_info['link_atom'], gly_link_res)
 
 for residue in target_res_list:
 
@@ -102,34 +164,27 @@ for residue in target_res_list:
     str_res_no = str(res_no)
 
     # O-links only occur on SER and THR residues
-    # N- links at ASN residues
+    # N-links at ASN residues
     # Check to see that a valid residue type was specified
+    
     target_residue = target_structure + " and chain " + chain_id + " and resi " + str_res_no
-    res_type = pymol.cmd.get_model(target_residue + " and chain " + chain_id, 1).atom[0].resn
+    link_info = get_protein_link_info(target_residue)
 
-    if res_type in ['SER', 'THR', 'ASN']:
-
-        # Load template glycan (o-linked to a SER or THR as appropriate)
-        # The name of the resulting pymol pbject is stored in glycan_structure
-        if res_type == 'SER':
-            glycan_pdb = os.path.join(args.glycan_path,'ser_o-link.pdb')
-            identifier = chain_id + str_res_no + 'OG'
-        elif res_type == 'THR':
-            glycan_pdb = os.path.join(args.glycan_path,'thr_o-link.pdb')
-            identifier = chain_id + str_res_no + 'OG1'
-        else:
-            glycan_pdb = os.path.join(args.glycan_path,'n-link.pdb')
-            identifier = chain_id + str_res_no + 'ND2'
+    if link_info:
+        
+        target_res_selection = pymol.cmd.get_model(target_residue + " and chain " + chain_id, 1)
+        res_type = target_res_selection.atom[0].resn
+        seg_id = target_res_selection.atom[0].segi
             
         glycan_structure = 'glycan'
-        pymol.cmd.load(glycan_pdb, glycan_structure)
+        pymol.cmd.load(os.path.join(args.glycan_path,link_info['template']), glycan_structure)
 
         # Align the linked residue in the template with the target residue
         # using the atoms selected in atom_mask
         target_align_selection = target_residue + atom_mask
-        linkResidue = glycan_structure + " and resn " + res_type
+        linkResidue = glycan_structure + " and resn " + link_info['link_res']
         link_res_selection = linkResidue + atom_mask
-        glycan_selection = glycan_structure + " and (not resn " + res_type + ")"
+        glycan_selection = glycan_structure + " and (not resn " + link_info['link_res'] + ")"
         pymol.cmd.align(link_res_selection, target_align_selection)
 
         # Remove the link residue in the template
@@ -137,6 +192,7 @@ for residue in target_res_list:
 
         # Alter the chain_id of the template glycan
         pymol.cmd.alter(glycan_structure, "chain = '" +  chain_id + "'")
+        pymol.cmd.alter(glycan_structure, "segi = '" +  seg_id + "'")
         
         # Give the glycan residues a residue number after that of the existing atoms
         # of the chain containing the target link residue
@@ -144,14 +200,18 @@ for residue in target_res_list:
         first_glycan_res = int(pymol.cmd.get_model(glycan_selection,1).atom[0].resi)
         last_glycan_res = int(pymol.cmd.get_model(glycan_selection,1).atom[-1].resi)
 
-        gly2prot_link[chain_id + str(new_res_no)] = identifier
-
         for old_res_no in range(first_glycan_res, last_glycan_res + 1):
             pymol.cmd.alter(glycan_selection + ' and resi ' + str(old_res_no), 'resi = ' + str(new_res_no))
             new_res_no += 1
 
         # Create a new structure combining the original target structure and the added glycan
         pymol.cmd.create('glycan_added', glycan_structure + ' or ' + target_structure)
+        
+        # Ensure that the glycan-protein linkage is incorporated in the new model
+        new_pro_link = 'glycan_added and chain ' + chain_id + ' and resi ' + str_res_no
+        new_gly_link = 'glycan_added and chain ' + chain_id + ' and resi ' + str(new_res_no)
+        create_gly_link(new_pro_link, link_info['link_atom'], new_gly_link)
+        
         
         # Delete both old structures
         pymol.cmd.delete(glycan_structure)
@@ -167,69 +227,7 @@ for residue in target_res_list:
 # Sort and save the glycosylated structure
 pymol.cmd.sort(target_structure)
 
-pymol.stored.glycan_residues = []
-
-# Create a list of all glycan residues
-# Identify by the concatenation of the chain and residue number
-pymol.cmd.iterate("not pol and name C1","stored.glycan_residues.append(chain + resi)")
-
 pymol.cmd.save(args.out_pdb, target_structure + ' and not hydro')
-
-out = open('tmp.pdb','w')
-
-# Get a list of protein link atom identifiers (ChainResidAtomname)
-protein_link_atoms = gly2prot_link.values()
-# Get the glycan residues involved in the link to the protein (ChainResid)
-glycan_link_res = gly2prot_link.keys()
-
-# List to contain the index values of all glycan residues
-# All this farago is necessary as pymol creates TER cards with 
-# atom numbers/index in the PDB output but doesn't store them internally.
-# Consequently the indices used in pymol differ from those in the output.
-glycan_index = []
-
-# Dictionary: Key = glycan link atom, Value = protein link residue index
-glycan_link_index = {}
-# Dictionary: Key = protein link atom identifier (ChainResidAtomname),  Value = protein link atom index
-prot_link_index = {}
-
-with open(args.out_pdb, 'r') as f:
-    for line in f:
-        # Identify and record atoms within glycan residues
-        # We need to keep CONECT records associated with them
-        if (line[0:6] in ['ATOM  ', 'HETATM']):
-            # chain_res = ChainResid
-            chain_res = line[21] + line[22:26].strip()
-            index = line[6:11]
-            
-            if chain_res in pymol.stored.glycan_residues:
-                
-                glycan_index.append(index)
-                
-                if (chain_res in glycan_link_res) and line[12:16].strip() == 'C1':
-                    # For the C1 atom of link residues we need to record the
-                    # index of the linked protein atom in a dictionary with the
-                    # C1 atosm index as the key
-                    glycan_link_index[index] = prot_link_index[gly2prot_link[chain_res]]
-                    
-            elif chain_res + line[12:16].strip() in protein_link_atoms:
-                
-                prot_link_index[chain_res + line[12:16].strip()] = index
-                
-            out.write(line)   
-        elif line[0:6] == 'CONECT':
-            # Keep connect records for the identified glycan atoms
-            if line[6:11] in glycan_index:
-                if line[6:11] in glycan_link_index:
-                    # Add in the link with the protein protein
-                    out.write(line[:-1] + glycan_link_index[line[6:11]] + '\n')
-                else:
-                    out.write(line)
-        else:
-            out.write(line)
-out.close()
-
-os.rename('tmp.pdb',args.out_pdb)
 
 # Get out!
 pymol.cmd.quit()
